05f6c909c65f86e3c9dfd3aa0e89269dcda5c92e
Dana Koeppe <koeppeda@msu.edu>
2018-05-02 14:53:09 -0400

    added functioning slit cutout, updated ycoords txt

--- Slit_id.py
+++ Slit_id.py
@@ -8,6 +8,7 @@ import pyregion
 import matplotlib.pyplot as plt
 import regions
 import matplotlib.cm as cm
+from PIL import Image as P
 
 """
 
@@ -35,6 +36,8 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
     data,header = fits.getdata(input,header=True)
     sz = data.shape
     N_pixel_x = sz[1] #number of x pixels
+    N_pixel_y = sz[0]
+
    #print(sz)
     #print(data[2249-15:2249+15,0:19])
     x_edges_main = []
@@ -42,19 +45,23 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
 
     for approx_edge in approx_edges:
 
-        starting_pixel = 0
+        starting_pixel = N_pixel_x-1
         x_edge = []
         y_edge = []
         previous_ycoord = approx_edge
         #print(previous_ycoord)
 
 
-        while starting_pixel < N_pixel_x:
+        while starting_pixel >= 0: #< N_pixel_x:
 
+            end_pixel = np.asarray([starting_pixel - binsize-1,0]).max()
 
-            end_pixel = np.asarray([starting_pixel + binsize-1,N_pixel_x-1]).min()
+            #end_pixel = np.asarray([starting_pixel + binsize-1,N_pixel_x-1]).min()
+
             cutout_bin = data[np.int(previous_ycoord) - np.int(cutout_size/2): np.int(previous_ycoord) + np.int(cutout_size/2),\
-                        np.int(starting_pixel) : np.int(end_pixel)]
+                        np.int(end_pixel) : np.int(starting_pixel)]
+            #cutout_bin = data[np.int(previous_ycoord) - np.int(cutout_size/2): np.int(previous_ycoord) + np.int(cutout_size/2),\
+            #            np.int(starting_pixel) : np.int(end_pixel)]
             #print(cutout_bin)
 
             profile = np.median(cutout_bin, axis=1)
@@ -63,17 +70,35 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
             derivative[0] = 0
             derivative[-1] = 0
             peak,peak_location = derivative.max(),np.argmax(derivative)
+            print("previous ycoord - cutout/2:previous ycoord + cutout/2=[%s:%s]"\
+            %(np.int(previous_ycoord) - np.int(cutout_size/2),np.int(previous_ycoord) + np.int(cutout_size/2)) )
+            print("starting pixel %s"%(starting_pixel))
+            print("end pixel %s"%(end_pixel))
+            print("peak location: %s"%(peak_location))
 
 
             peak_location += (previous_ycoord - (cutout_size/2))
+            print("peak location updated: %s"%(peak_location))
 
-            x_edge.append(int(np.round(0.5*(starting_pixel + end_pixel),0)))
-            y_edge.append(int(peak_location))
+            x_edge.insert(0,int(np.round(0.5*(starting_pixel + end_pixel),0)))
+            #x_edge.append(int(np.round(0.5*(starting_pixel + end_pixel),0)))
+            y_edge.insert(0,int(peak_location))
+            #y_edge.append(int(peak_location))
+            #print(previous_ycoord)
 
             previous_ycoord = peak_location
 
-            starting_pixel += binsize
+            starting_pixel -= binsize
+            if starting_pixel<10:
+                x_edge.insert(0,0)
+                x_edge.append(N_pixel_x)
+                y_edge.insert(0,previous_ycoord)
+                last_y = y_edge[-1]
+                y_edge.append(last_y)
+                break
 
+        #x_edges_main.insert(0,x_edge)
+        #y_edges_main.insert(0,y_edge)
         x_edges_main.append(x_edge)
         y_edges_main.append(y_edge)
 
@@ -94,10 +119,40 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
 
     #hdu = fits.open('LMask1/flat_fielded/fnLMask18150c1.fits')
 
+    #print(data[x_edges_main,y_edges_main])
 
-    str_tops = []
-    str_bottoms = []
+    x_edges_main, y_edges_main = np.asarray(x_edges_main),np.asarray(y_edges_main)
+    #print(x_edges_main.shape,y_edges_main.shape)
+    #print(y_edges_main)
 
+    """
+    slope,yint = np.polyfit(x_edges_main[0],y_edges_main[0],1)
+    print(slope,yint)
+
+
+    y_top = np.dot(np.asarray((slope*x) + yint).astype(int),np.ones(len(y_edges_main[0])))
+    y_bottom = np.dot(np.asarray((slope*x)+ yint).astype(int)-21,np.ones(len(y_edges_main[0])))
+
+
+
+
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    #ax.imshow(data, cmap=cm.gray,origin="lower")
+    logscale_data = np.log(1000*data + 1)/np.log(1000)
+    #ax.imshow(logscale_data, cmap=cm.gray,origin="lower")
+    ax.plot(x,y)
+
+    print(x,y)
+
+    ax.imshow(logscale_data[y[-1]-30:y[0]+30,x[0]-2:x[-1]+2],cmap=cm.gray,origin="lower")
+
+
+
+    plt.show()
+    """
+
+    polygons = []
     for i in range(len(x_edges_main)):
 
 
@@ -106,11 +161,12 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
         xtf,ytf = int(xt[-1]),int(yt[-1])
         xbi,ybi = xti,yti-21
         xbf,ybf = xtf,ytf-21
-        str_top ='''line('''+str(xti)+","+str(yti)+","+str(xtf)+","+str(ytf)+''') # line=0 0 color=blue\n'''
-        str_bottom = '''line('''+str(xbi)+","+str(ybi)+","+str(xbf)+","+str(ybf)+''') # line=0 0 color=red\n'''
+        box = '''polygon('''+str(xti)+","+str(yti)+","+str(xtf)+","+str(ytf)+","+str(xbf)+","+str(ybf)+","+str(xbi)+","+str(ybi)+''') \
+        # line=0 0 color=blue\n'''
 
-        str_tops.append(str_top)
-        str_bottoms.append(str_bottom)
+
+        polygons.append(box)
+
 
 
 
@@ -119,64 +175,34 @@ def get_edges(input,slit_txt,mask,cutout_size=30,binsize=20):
     """global color=green dashlist=8 3 width=2 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 """+\
     """fixed=0 edit=1 move=1 delete=1 include=1 source=1\n physical \n""")
 
-    for i in range(len(str_tops)):
-        region_txt.write(str(str_tops[i])+str(str_bottoms[i]))
+    for i in range(len(polygons)):
+        #region_txt.write(str(str_tops[i])+str(str_bottoms[i]))
+        region_txt.write(str(polygons[i]))
 
 
     region_txt.close()
 
     reg_string = open("%s/reg_txt.reg"%mask,"r").read()
 
+
     #r = regions.write_ds9(reg_string,region_filename)
 
-    fig = plt.figure()
-    ax = plt.subplot(111)
-    ax.imshow(data, cmap=cm.gray,origin="lower")
-
-    r2 = pyregion.parse(reg_string).as_imagecoord(header=header)
+    return x_edges_main,y_edges_main
 
 
-    patch_list, artist_list = r2.get_mpl_patches_texts()
 
+def trace_em_lines():
+    #this function will try to trace the edges of slites based on arclamp emission lines.
+    #arclamp used for this will be LMask2/flat_fielded/fnLMask28160c1.fits
+    input = "LMask2/flat_fielded/fnLMask28160c1.fits"
+    image,header = fits.getdata(input,header=True)
+    ymargin = 2
+    lower_edge = (504-ymargin)
+    cutout = image[lower_edge: 527+ymargin,:]
+    Nx = cutout.shape[1]
+    Ny = cutout.shape[0]
 
-    for p in patch_list:
-        ax.add_patch(p)
-    for t in artist_list:
-        ax.add_artist(t)
-
-    logscale_data = np.log(1000*data + 1)/np.log(1000)
-    ax.imshow(logscale_data, cmap=cm.gray,origin="lower")
-
-
-    plt.show()
-
+    dist_to_center  = np.abs(np.arange(Ny) - Ny/2)
+    profile = np.median(cutout,axis=0)
 
     return
-
-
-
-'''
-hdu = fits.PrimaryHDU(out_data.astype("f"))
-hdu.header = header.copy()
-hdu.header.add_history('One slit extracted')
-hdu.writeto('LMask1/single_slit.fits',overwrite=True)
-'''
-
-
-"""
-data,header = fits.getdata('LMask1/flat_fielded/fnLMask18150c1.fits',header=True)
-
-N_pix_x = data.shape[1]
-
-num_slits = 1
-space_top = 84.0 #empty space between top slit and edge of detector in pixels
-space_between_slits = 2.0
-bar_height = 22.0
-index_first_bar = 1
-index_last_bar = 2
-
-slit_top = 1500 - space_top - bar_height * (index_first_bar-1.0) - 0.5*space_between_slits
-slit_bottom = 1500 - space_top - bar_height*(index_last_bar) + 0.5*space_between_slits
-
-target_position = 0.5*(slit_bottom+slit_top)
-"""
